1e5db82675af253b8e2754c91bda6bf72048a6ca
Martin SCHREIBER <schreiberx@gmail.com>
2021-04-14 00:01:51 +0200

    u

--- python_config.sh
+++ python_config.sh
@@ -8,6 +8,6 @@ SCRIPTDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 #
 # Directory of Python virtual environment
 #
-#VENV_DIR="$SCRIPTDIR/python_venv/"
-VENV_DIR="$HOME/python_venvs/libpdefd"
+VENV_DIR="$SCRIPTDIR/python_venv__$(hostname)/"
+#VENV_DIR="$HOME/python_venvs/libpdefd"
 
