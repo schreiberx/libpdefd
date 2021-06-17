a86610971d43c27a71ddf1eea93e595b99746f64
Martin SCHREIBER <schreiberx@gmail.com>
2021-06-17 20:21:00 +0200

    u

---
+++ run_ex_d02_tests_vertical_straka_rk4_2021_06_08/postprocessing_4_pot_t_diff.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+BENCHMARK_NAME=vertical_straka
+TIME_INTEGRATOR=rk4
+
+if [ "#$1" == "#" ]; then
+	ARGS=""
+	ARGS+=" --halt now,fail=1"
+	#ARGS+=" --halt never"
+	ARGS+=" --eta"
+	ARGS+=" --bar"
+	ARGS+=" --progress"
+
+	# Limit to 10 Jobs because of memory constraints
+	FREEMEMMB=`free -m | grep "^Mem:" | awk '{print $2}'`
+	MAXJOBS=$((FREEMEMMB/(1024*2)))
+
+	echo "Limiting to $MAXJOBS jobs due to memory constraints"
+	ARGS+=" -j $MAXJOBS"
+
+	#ARGS+=" ::: "
+
+	# Deactivate multithreading
+	export MKL_NUM_THREADS=1
+	export OMP_NUM_THREADS=1
+
+	./$0 print_parallel_jobs || exit 1
+
+	./$0 print_parallel_jobs | parallel $ARGS
+
+	exit 0
+fi
+
+
+if [ "#$1" == "#print_parallel_jobs" ]; then
+	for DIR in output_plot__*/; do
+		for VARNAME in pot_t_diff; do
+			OUTPUT_PREFIX=""
+
+			TIMESTAMP=00000900
+			FILE="${DIR}/output_idx_${VARNAME}_${TIMESTAMP}.png"
+			echo cp "$FILE" "output_image_${DIR}_${VARNAME}_${TIMESTAMP}.png"
+		done
+	done
+	exit 0
+fi
+
+echo "ERROR"
+exit 1
+
